7c2f470237231d0cac06c5f572dd06c6fb317b0e
syrgabek <syrgabekakparaliev.9@gmail.com>
2022-05-18 21:47:00 +0600

    python project

--- book/models.py
+++ book/models.py
@@ -10,11 +10,11 @@ class Book(models.Model):
     )
     title = models.CharField(max_length=50)
     description = models.TextField()
-    createa_data= models.DateField(auto_now_add=True)
-    updeted_data = models.DateField(auto_now=True)
+    created_date= models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
     image = models.ImageField(upload_to='')
     author = models.CharField(max_length=25, null=True)
-    gengre = models.CharField(max_length=90, choices=GENRE, null=True)
+    genre = models.CharField(max_length=90, choices=GENRE, null=True)
 
     def __str__(self):
         return self.title
